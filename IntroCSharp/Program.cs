14665df96b337f0cd302c0bf9785f9d23d611905
Joseph Fisher <josephfisherbusiness@gmail.com>
2019-08-13 09:44:18 -0700

    My work that I've completed

---
+++ IntroCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This really do be a perter griffin moment 17");
+            Console.ReadLine();
+        }
+    }
+}
